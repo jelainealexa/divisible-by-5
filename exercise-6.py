aac2bd9670006c2e9b09b6980576a1fde5f909b1
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-22 19:13:48 +0800

    Final

--- exercise-6.py
+++ exercise-6.py
@@ -1,10 +1,11 @@
 # Display numbers divisible by 5 from a list
 
+# Given
+numbers = [3, 5, 10, 14, 26, 40]
+
 # Iterate the given list of numbers
-for i in numbers:
+for number in numbers:
     # Check if the number is divisible by 5
     if number % 5 == 0:
         # Print the number
         print(number)
-
-# Given
